21344e6d984754615c116950182c8b48688e5b58
Dorit <sg.sapir@gmail.com>
2021-05-25 15:16:16 +0300

    Add notes in the code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from collections import Counter
 from string import punctuation
 
+
 def wordStor(site):
     # We get the url
     r = requests.get(site)
@@ -16,11 +17,12 @@ def wordStor(site):
     text_div = (''.join(s.findAll(text=True)) for s in soup.findAll('div'))
     c_div = Counter((x.rstrip(punctuation).lower() for y in text_div for x in y.split()))
 
-    # We sum the two countesr and get a list with words count from most to less common
+    # We sum the two counter and get a list with words count from most to less common
     total = c_div + c_p
     list_most_common_words = total.most_common(50)
 
     return list_most_common_words
+
 
 def addNextLowNum(full_list, search_value, indx):
     # Full_list is ordered from high occurrences to low,
@@ -32,25 +34,28 @@ def addNextLowNum(full_list, search_value, indx):
     for i, lst in enumerate(full_list):
         # Keep the index of the search_value
         if lst[1] <= search_value[1]:
-          # First or in the middle
-          full_list.insert(i, search_value)
-          return (full_list)
-    # At rge end of a list
+            # First or in the middle
+            full_list.insert(i, search_value)
+            return (full_list)
+    # At the end of a list
     full_list.append(search_value)
     return (full_list)
 
+
 def clnGarbdge(fullList):
-    # Decidion: len of 1 char that is not currency
-    values_list = [(k,r[0]) for k, r in enumerate(fullList) if len(r[0]) < 2]
-    accepted_list = ['$','₪','£','¥',' ₣' ,'₤' ,'₧' ,'€' ,'₹' ,'₩' ,'₴' ,'₯','₮','₰' ,'₲', '₱', '₳', '₵' ,'₭' ,'₫']
-    #print(values_list)
+    # Decision: len of 1 char that is not currency
+    values_list = [(k, r[0]) for k, r in enumerate(fullList) if len(r[0]) < 2]
+    accepted_list = ['$', '₪', '£', '¥', ' ₣', '₤', '₧', '€', '₹', '₩', '₴', '₯', '₮', '₰', '₲', '₱', '₳', '₵', '₭',
+                     '₫']
+    # print(values_list)
 
     values = len(values_list)
     while values:
-        if not values_list[values-1][1] in accepted_list:
-            del fullList[values_list[values-1][0]]
-        values -=1
+        if not values_list[values - 1][1] in accepted_list:
+            del fullList[values_list[values - 1][0]]
+        values -= 1
     return fullList
+
 
 if __name__ == '__main__':
     arr = ['http://he.wikipedia.org', 'http://ynet.co.il', 'http://www.talniri.co.il']
@@ -60,14 +65,14 @@ if __name__ == '__main__':
     for web_page in arr:
         # For each web page Return a list of tuple that include count of words in the web page
         word_tuple = wordStor(web_page)
-        #print("New words list", len(word_tuple), word_tuple)
+        # print("New words list", len(word_tuple), word_tuple)
         # Merge lists
         if not over_all:
             # When the over_all list is empty
             over_all = word_tuple.copy()
             continue
 
-        # handle addition of elements while combine the word occurrences from all sites
+        # Handle addition of elements while combine the word occurrences from all sites
         for input_value in word_tuple:
             # Combine the word numerator into one ordered list
             values_list = [r[0] for r in over_all]
@@ -76,22 +81,22 @@ if __name__ == '__main__':
             search = input_value
 
             if indx != -1:
-                # for exists word -  add occurrences
+                # For exists word -  add occurrences
                 tmp = (input_value[0], input_value[1] + over_all[indx][1])
                 search = tmp
 
-            addNextLowNum(over_all, search,indx)
-        #print("Over all list, each round", len(over_all), over_all)
+            addNextLowNum(over_all, search, indx)
+        # print("Over all list, each round", len(over_all), over_all)
 
+    # Deal with clean data
     clnGarbdge(over_all)
-    k=len(over_all)
-    # print word by its numerator from lower to higher (reverse)
-    # reverse operation is costly then print reverse
+
+    # Print word by its numerator from lower to higher (reverse)
+    # Reverse operation is costly then print reverse
+    k = len(over_all)
+
     while k:
-        print("length",over_all[-1][1], end ="")
-        print( ":", over_all[-1][0])
-        k-=1
+        print("length", over_all[-1][1], end="")
+        print(":", over_all[-1][0])
+        k -= 1
         over_all = over_all[:k]
-
-
-
